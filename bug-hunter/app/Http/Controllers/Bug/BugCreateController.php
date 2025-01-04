440caae1c660e9e186ea09ade38d8e010e504d1c
athira-shibu <athira.shibu@dibasys.com>
2025-01-04 15:34:11 +0530

    created bug creation controller

--- bug-hunter/app/Http/Controllers/Bug/BugCreateController.php
+++ bug-hunter/app/Http/Controllers/Bug/BugCreateController.php
@@ -3,24 +3,51 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Bug;
 
+use App\Constants\Bug\BugSeverityEnum;
+use App\Constants\Bug\BugStatusEnum;
+use App\DataTransferObjects\Bug\BugCreateDto;
+use App\Exceptions\Category\CategoryNotFoundException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Bug\BugCreateRequest;
+use App\Models\Category\Category;
+use App\Repositories\Interfaces\Bug\BugRepositoryInterface;
 use App\Repositories\Interfaces\Category\CategoryRepositoryInterface;
+use DateTime;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class BugCreateController extends Controller
 {
     private CategoryRepositoryInterface $categoryRepository;
 
+    private BugRepositoryInterface $bugRepository;
+
     public function __construct(
-        CategoryRepositoryInterface $categoryRepository
+        CategoryRepositoryInterface $categoryRepository,
+        BugRepositoryInterface $bugRepository
     ) {
         $this->categoryRepository = $categoryRepository;
+        $this->bugRepository = $bugRepository;
     }
 
     public function create(BugCreateRequest $request, int $id): JsonResponse
     {
         $category = $this->categoryRepository->findOneById($id);
-        return new JsonResponse();
+
+        if ($category instanceof Category ===false) {
+            throw new CategoryNotFoundException('category not found.');
+        }
+
+        $createDto = new BugCreateDto(
+            $request->get('title'),
+            $request->get('description'),
+            new BugSeverityEnum($request->get('severity')),
+            new BugStatusEnum($request->get('status')),
+            new DateTime($request->get('raised_at'))
+        );
+
+        $bug = $this->bugRepository->create($createDto, $category);
+
+        return new JsonResponse($bug->toArray(), Response::HTTP_CREATED);
     }
 }
