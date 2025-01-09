729fa185404f8a767d05f85c0482cd8ecb3083cb
athira-shibu <athira.shibu@dibasys.com>
2025-01-09 21:43:19 +0530

    added company logo

--- bug-hunter/resources/js/components/SidebarComponent.vue
+++ bug-hunter/resources/js/components/SidebarComponent.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="sidebar">
+        <!-- my company logo -->
+         <div class="logo">
+            <img src="assets\images\company logo.png" alt="Company logo" />
+         </div>
         <ul>
-            <li>Blogs</li>
+            <li>Bugs</li>
             <li>Categories</li>
             <li>Members</li>
             <li>Get AI Help</li>
+            <li>Analysis</li>
         </ul>
     </div>
 </template>
