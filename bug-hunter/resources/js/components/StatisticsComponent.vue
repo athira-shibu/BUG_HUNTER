af5958699687f3a3b0b60ee54513af57351c79db
athira-shibu <athira.shibu@dibasys.com>
2025-01-10 07:26:54 +0530

    styled stats

--- bug-hunter/resources/js/components/StatisticsComponent.vue
+++ bug-hunter/resources/js/components/StatisticsComponent.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="stats">
-        <h2>120</h2>
-        <p>Bugs Reported</p>
+        <div>
+            <h2>120</h2>
+            <p>Bugs Reported</p>
+        </div>
+        <div>
+            <h2>80</h2>
+            <p>Bugs Resolved</p>
+        </div>
+        <div>
+            <h2>40</h2>
+            <p>Open Bugs</p>
+        </div>
     </div>
 </template>
