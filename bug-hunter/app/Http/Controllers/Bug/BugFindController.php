3c242f7b9a1335cc2a8d2d3edb66bdfb4158327c
athira-shibu <athira.shibu@dibasys.com>
2025-01-13 21:08:09 +0530

    added bug find endpoint

---
+++ bug-hunter/app/Http/Controllers/Bug/BugFindController.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Bug;
+
+use App\Exceptions\Bug\BugNotFoundException;
+use App\Http\Controllers\Controller;
+use App\Models\Bug\Bug;
+use App\Repositories\Interfaces\Bug\BugRepositoryInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+final class BugFindController extends Controller
+{
+    private BugRepositoryInterface $bugRepository;
+
+    public function __construct(
+        BugRepositoryInterface $bugRepository
+    ) {
+        $this->bugRepository = $bugRepository;
+    }
+
+    public function find(string $id): JsonResponse
+    {
+        $bug = $this->bugRepository->findOneById($id);
+
+        if ($bug instanceof Bug === false) {
+            throw new BugNotFoundException(\sprintf('Bug for id - %s not found.', $id));
+        }
+
+        return new JsonResponse($bug->toArray(), Response::HTTP_OK);
+    }
+}
