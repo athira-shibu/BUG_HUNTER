7ca9566ed69a2aaf7aec29807e8f857636436520
athira-shibu <athira.shibu@dibasys.com>
2025-02-14 17:01:58 +0530

    added icons in sidebar

--- bug-hunter/resources/js/components/SidebarComponent.vue
+++ bug-hunter/resources/js/components/SidebarComponent.vue
@@ -5,11 +5,11 @@
             <img src="assets\images\company logo.png" alt="Company logo" />
          </div>
         <ul>
-            <li><router-link to="/bug-list">Bugs</router-link></li>
-            <li><router-link to="/category-list">Categories</router-link></li>
-            <li>Members</li>
-            <li>Get AI Help</li>
-            <li>Analysis</li>
+            <li><router-link to="/bug-list">🪲bugs</router-link></li>
+            <li><router-link to="/category-list">📂categories</router-link></li>
+            <li>👥members</li>
+            <li>🤖Get AI Help</li>
+            <li>📊analysis</li>
         </ul>
     </div>
 </template>
